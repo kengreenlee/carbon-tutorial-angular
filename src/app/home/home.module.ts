9ecc0331a76c9c85e2fdc406b8dc85ffbe4d04c3
Ken Greenlee <kgreenle@us.ibm.com>
2022-12-02 15:22:20 -0500

    init

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeRoutingModule } from './home-routing.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { BreadcrumbModule, GridModule, TabsModule } from 'carbon-components-angular';
+
+@NgModule({
+    declarations: [
+        LandingPageComponent
+    ],
+    imports: [
+        CommonModule,
+        HomeRoutingModule,
+        GridModule,
+        BreadcrumbModule,
+        TabsModule
+    ]
+})
+export class HomeModule { }
